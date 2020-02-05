a1793fe98fb0865ca786a6e05a2e640225e64e8d
andres <condeandres34@gmail.com>
2020-02-05 20:43:40 -0300

    registraaaaaa

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -66,7 +66,7 @@ class RegisterController extends Controller
 
     protected function create(array $data)
     {
-      dd($data);
+      
 
       if(strpos($data['email'], 'admin')){
         return User::create([
