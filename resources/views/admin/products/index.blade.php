07ae4eb896c9dc5a50c0ffbca61b6441169f0117
mc-sanchez <55860987+mc-sanchez@users.noreply.github.com>
2020-02-05 23:31:36 -0300

    "Ultimos cambiosssss"

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -8,6 +8,7 @@
         <div class="col-md-4">
           <div class="row">
               <img src="{{ Storage::url($product->image) }}" alt="Menu">
+              <img src="{{ Storage::url($product->image) }}" alt="Menu" max-width="100px">
               <h3>{{$product->name}}</h3>
               <p class="grey-text">{{$product->description}}</p>
               <a class="btn btn-outline-dark btn-sm" href="products/{{$product->id}}">Ver detalle</a>
